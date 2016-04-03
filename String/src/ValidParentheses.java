d01ea5ddf8e9abcdb8c66e965507dbccd2f199c4
karolQ <yingkunli512@gmail.com>
2016-04-03 19:33:35 -0400

    20. Valid Parentheses Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
    The brackets must close in the correct order, "()" and "()[]{}" are all valid but "(]" and "([)]" are not.
    
    Subscribe to see which companies asked this question
    
    (Using stack)

---
+++ String/src/ValidParentheses.java
@@ -0,0 +1,37 @@
+import java.util.Stack;
+
+public class ValidParentheses {
+	public static void main(String[] args){
+		String s1 = " ";
+		String s2 = "(){[]}";
+		String s3 = "({)[]}";
+		boolean res1 = isValid(s1);
+		boolean res2 = isValid(s2);
+		boolean res3 = isValid(s3);
+		System.out.println(res1);
+		System.out.println(res2);
+		System.out.println(res3);
+	}
+	
+	public static boolean isValid(String s){
+		if (s == null || s.length() % 2 == 1)
+			return false;
+		Stack<Character> stack = new Stack<>();
+		for (int i = 0; i < s.length(); i++){
+			char c = s.charAt(i);
+			if (c == '(' || c == '[' || c == '{')
+				stack.push(c);
+			else{
+				if (stack.isEmpty())
+					return false;
+				else if (c == ')' && stack.pop() != '(')
+					return false;
+				else if (c == ']' && stack.pop() != '[')
+					return false;
+				else if (c == '}' && stack.pop() != '{')
+					return false;
+			}
+		}
+		return stack.isEmpty();
+	}
+}
